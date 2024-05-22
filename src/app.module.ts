66f34002e0c4355132ae7756b850dcd5fe0d8217
Albara Hassan <braa3300@gmail.com>
2024-05-22 16:25:37 +0200

    fix: removed unneded controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [CoreModule, AuthModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
